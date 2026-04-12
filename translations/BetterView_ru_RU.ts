1003539f987c34a208759b82682f93a6d475c344
antonlosk <antonloskutovqwerty@gmail.com>
2026-04-12 17:17:06 +0500

    Add files via upload

---
+++ translations/BetterView_ru_RU.ts
@@ -0,0 +1,242 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>Toolbar</source>
+        <translation>Панель инструментов</translation>
+    </message>
+    <message>
+        <source>Open (Ctrl+O)</source>
+        <translation>Открыть (Ctrl+O)</translation>
+    </message>
+    <message>
+        <source>↺</source>
+        <translation>↺</translation>
+    </message>
+    <message>
+        <source>Rotate Left</source>
+        <translation>Повернуть влево</translation>
+    </message>
+    <message>
+        <source>↻</source>
+        <translation>↻</translation>
+    </message>
+    <message>
+        <source>Rotate Right</source>
+        <translation>Повернуть вправо</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <source>No file</source>
+        <translation>Нет файла</translation>
+    </message>
+    <message>
+        <source>Zoom Out</source>
+        <translation>Уменьшить</translation>
+    </message>
+    <message>
+        <source>Zoom In</source>
+        <translation>Увеличить</translation>
+    </message>
+    <message>
+        <source>⛶</source>
+        <translation>⛶</translation>
+    </message>
+    <message>
+        <source>Fullscreen (F11)</source>
+        <translation>Полный экран (F11)</translation>
+    </message>
+    <message>
+        <source>◀</source>
+        <translation>◀</translation>
+    </message>
+    <message>
+        <source>Previous (Left Arrow)</source>
+        <translation>Предыдущее (←)</translation>
+    </message>
+    <message>
+        <source>▶</source>
+        <translation>▶</translation>
+    </message>
+    <message>
+        <source>Next (Right Arrow)</source>
+        <translation>Следующее (→)</translation>
+    </message>
+    <message>
+        <source>⋮</source>
+        <translation>⋮</translation>
+    </message>
+    <message>
+        <source>Menu</source>
+        <translation>Меню</translation>
+    </message>
+    <message>
+        <source>Theme</source>
+        <translation>Тема</translation>
+    </message>
+    <message>
+        <source>System</source>
+        <translation>Системная</translation>
+    </message>
+    <message>
+        <source>Dark</source>
+        <translation>Тёмная</translation>
+    </message>
+    <message>
+        <source>Light</source>
+        <translation>Светлая</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation>Выход</translation>
+    </message>
+    <message>
+        <source>About Better View</source>
+        <translation>О Better View</translation>
+    </message>
+    <message>
+        <source>&lt;h3&gt;Better View&lt;/h3&gt;&lt;p&gt;Version: %1&lt;/p&gt;&lt;p&gt;Image Viewer for Windows 10&lt;/p&gt;&lt;p&gt;Author: &lt;b&gt;antonlosk&lt;/b&gt;&lt;/p&gt;&lt;p&gt;GitHub: &lt;a href=&quot;https://github.com/antonlosk/Better-View&quot;&gt;https://github.com/antonlosk/Better-View&lt;/a&gt;&lt;/p&gt;</source>
+        <translation>&lt;h3&gt;Better View&lt;/h3&gt;&lt;p&gt;Версия: %1&lt;/p&gt;&lt;p&gt;Просмотрщик изображений для Windows 10&lt;/p&gt;&lt;p&gt;Автор: &lt;b&gt;antonlosk&lt;/b&gt;&lt;/p&gt;&lt;p&gt;GitHub: &lt;a href=&quot;https://github.com/antonlosk/Better-View&quot;&gt;https://github.com/antonlosk/Better-View&lt;/a&gt;&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+    <message>
+        <source>Cannot load file:\n%1</source>
+        <translation>Не удалось загрузить файл:\n%1</translation>
+    </message>
+    <message>
+        <source>Cannot load GIF file:\n%1</source>
+        <translation>Не удалось загрузить GIF-файл:\n%1</translation>
+    </message>
+    <message>
+        <source>Images (*.jpg *.jpeg *.jpe *.png *.gif *.webp *.bmp *.ico *.tiff *.tif *.svg *.svgz *.heif)</source>
+        <translation>Изображения (*.jpg *.jpeg *.jpe *.png *.gif *.webp *.bmp *.ico *.tiff *.tif *.svg *.svgz *.heif)</translation>
+    </message>
+    <message>
+        <source>Open Image</source>
+        <translation>Открыть изображение</translation>
+    </message>
+    <message>
+        <source>Delete file?</source>
+        <translation>Удалить файл?</translation>
+    </message>
+    <message>
+        <source>Cannot delete file.</source>
+        <translation>Не удалось удалить файл.</translation>
+    </message>
+    <message>
+        <source>Not supported</source>
+        <translation>Не поддерживается</translation>
+    </message>
+    <message>
+        <source>File format not supported.</source>
+        <translation>Формат файла не поддерживается.</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <source>Theme:</source>
+        <translation>Тема:</translation>
+    </message>
+    <message>
+        <source>System</source>
+        <translation>Системная</translation>
+    </message>
+    <message>
+        <source>Light</source>
+        <translation>Светлая</translation>
+    </message>
+    <message>
+        <source>Dark</source>
+        <translation>Тёмная</translation>
+    </message>
+    <message>
+        <source>Language:</source>
+        <translation>Язык:</translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <translation>ОК</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Отмена</translation>
+    </message>
+    <message>
+        <source>Language Changed</source>
+        <translation>Язык изменён</translation>
+    </message>
+    <message>
+        <source>The language change will take effect after restarting the application.</source>
+        <translation>Изменение языка вступит в силу после перезапуска приложения.</translation>
+    </message>
+</context>
+<context>
+    <name>StatusBarManager</name>
+    <message>
+        <source>Resolution: -- x --</source>
+        <translation>Разрешение: -- x --</translation>
+    </message>
+    <message>
+        <source>Size: -- MB</source>
+        <translation>Размер: -- МБ</translation>
+    </message>
+    <message>
+        <source>Size: %1 MB</source>
+        <translation>Размер: %1 МБ</translation>
+    </message>
+    <message>
+        <source>Resolution: %1 x %2 (GIF)</source>
+        <translation>Разрешение: %1 x %2 (GIF)</translation>
+    </message>
+    <message>
+        <source>Resolution: -- x -- (GIF)</source>
+        <translation>Разрешение: -- x -- (GIF)</translation>
+    </message>
+    <message>
+        <source>Resolution: %1 x %2</source>
+        <translation>Разрешение: %1 x %2</translation>
+    </message>
+</context>
+<context>
+    <name>ZoomControl</name>
+    <message>
+        <source>Zoom:</source>
+        <translation>Масштаб:</translation>
+    </message>
+</context>
+<context>
+    <name>ImageViewer</name>
+    <message>
+        <source>Copy</source>
+        <translation>Копировать</translation>
+    </message>
+</context>
+<context>
+    <name>GifViewer</name>
+    <message>
+        <source>Copy</source>
+        <translation>Копировать</translation>
+    </message>
+</context>
+</TS>
